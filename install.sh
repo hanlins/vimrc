1189a0e0a27bea851efa91dc47fa1c970472c0e6
Hanlin Shi <shihanlin9@gmail.com>
2021-01-26 20:36:35 -0800

    Install extra tools and install golang

--- install.sh
+++ install.sh
@@ -16,6 +16,11 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim -y
+sudo apt-get install gcc -y
+sudo apt-get install make -y
+sudo apt-get install git -y
+sudo apt-get install bison -y
+sudo apt-get install silversearcher-ag -y
 
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -28,6 +33,13 @@ wget -O ~/.gitconfig https://raw.githubusercontent.com/hanlins/vimrc/master/.git
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/hanlins/vimrc/master/.tmux.conf
 mkdir -p .config/nvim/
 wget -O ~/.config/nvim/init.vim https://raw.githubusercontent.com/hanlins/vimrc/master/.vimrc
+
+# download gvm
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+echo "source /root/.gvm/scripts/gvm"
+source /root/.gvm/scripts/gvm
+gvm install go1.15.7 -B
+gvm use go.1.15.7
 
 # switch to zsh
 chsh -s $(which zsh)
