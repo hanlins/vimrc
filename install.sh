75c33965eedc329bb70ca080b63f972a6e812ca9
Hanlin Shi <shihanlin9@gmail.com>
2022-11-08 20:20:52 -0800

    Update install script for installing docker
    Signed-off-by: Hanlin Shi <shihanlin9@gmail.com>

--- install.sh
+++ install.sh
@@ -5,11 +5,17 @@ set -x
 # install zsh
 sudo apt-get install git -y
 sudo apt-get install zsh -y
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 bash ~/.fzf/install
+
+# install docker registry
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # install nvim
 sudo apt-get install software-properties-common -y
@@ -21,6 +27,12 @@ sudo apt-get install make -y
 sudo apt-get install git -y
 sudo apt-get install bison -y
 sudo apt-get install silversearcher-ag -y
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# add docker to docker user group
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
